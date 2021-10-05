e8e2b690eba1000308afd678a35a6dc663c0bd22
Saintlaw <Saintlaw@DESKTOP-7FF550F>
2021-10-05 13:00:00 +0100

    Interface and repository class added

---
+++ Vehicle.Data/Interface/IRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle.Data.Interface
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<ICollection<T>> FindAll();
+        Task<T> FindById(int id);
+        Task<bool> isExist(int id);
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<T> GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        Task<T> GetLastOrDefault(Expression<Func<T, bool>> predicate);
+        Task<T> GetSingleOrDefault(Expression<Func<T, bool>> predicate);
+        Task<bool> Save();
+    }
+}
---
+++ Vehicle.Data/Repository/PurchaseRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle.Data.Interface;
+using Vehicle.Data.Models;
+
+namespace Vehicle.Data.Repository
+{
+    public class PurchaseRepository : IPurchaseRepository
+    {
+        private readonly VehicleMgrContext _context;
+        public PurchaseRepository(VehicleMgrContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Create(Purchase entity)
+        {
+            await _context.Purchases.AnyAsync();
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Purchase entity)
+        {
+            _context.Purchases.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<ICollection<Purchase>> FindAll()
+        {
+            var record = await _context.Purchases.ToListAsync();
+            return record;
+        }
+
+        public async Task<Purchase> FindById(int id)
+        {
+            var record = await _context.Purchases.FindAsync(id);
+            return record;
+        }
+
+        public async Task<Purchase> GetFirstOrDefault(Expression<Func<Purchase, bool>> predicate)
+        {
+            return await _context.Purchases.FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<Purchase> GetLastOrDefault(Expression<Func<Purchase, bool>> predicate)
+        {
+            return await _context.Purchases.LastOrDefaultAsync(predicate);
+        }
+
+        public async Task<Purchase> GetSingleOrDefault(Expression<Func<Purchase, bool>> predicate)
+        {
+            return await _context.Purchases.SingleOrDefaultAsync(predicate);
+        }
+
+        public async Task<bool> isExist(int id)
+        {
+            var exist = await _context.Purchases.AnyAsync(x => x.Id == id);
+            return exist;
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _context.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> Update(Purchase entity)
+        {
+            _context.Purchases.Update(entity);
+            return await Save();
+        }
+    }
+}
